f732e10480f2b1c42ff062fa0cb91a8797ba3438
nikita-pokryshkin <nik.pokryshkin@gmail.com>
2020-08-29 15:24:21 +0300

    basket repository

---
+++ src/Basket/Basket.API/Entities/Repositories/BasketRepository.cs
@@ -0,0 +1,51 @@
+﻿using Basket.API.Data.Interfaces;
+using Basket.API.Entities.Repositories.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IBasketContext _context;
+
+        public BasketRepository(IBasketContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<BasketCart> GetBasketAsync(string userName)
+        {
+            var basket = await _context.Redis.StringGetAsync(userName);
+            if(basket.IsNullOrEmpty)
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<BasketCart>(basket);
+        }
+
+        public async Task<BasketCart> UpdateBasketAsync(BasketCart basket)
+        {
+            var updated = await _context.Redis.StringSetAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            
+            if (!updated)
+            {
+                return null;
+            }
+
+            return await GetBasketAsync(basket.UserName);
+        }
+
+        public async Task<bool> DeleteBasketAsync(string userName)
+        {
+            return await _context.Redis.KeyDeleteAsync(userName);
+        }
+
+    }
+}
---
+++ src/Basket/Basket.API/Entities/Repositories/Interfaces/IBasketRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities.Repositories.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<BasketCart> GetBasketAsync(string userName);
+        Task<BasketCart> UpdateBasketAsync(BasketCart basket);
+        Task<bool> DeleteBasketAsync(string userName);
+    }
+}
