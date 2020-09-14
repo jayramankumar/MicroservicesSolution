c2106506b3cbcf9d2231b539a6051662e3f958af
user-nik <nik.pokryshkin@gmail.com>
2020-09-14 19:14:49 +0300

    api gateway with ocelot lib

--- src/Ordering/Ordering.API/Controllers/OrderController.cs
+++ src/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Ordering.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class OrderController : ControllerBase
     {
